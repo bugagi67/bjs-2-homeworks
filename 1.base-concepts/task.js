66b4ae47f74540d5331c4ffea6f065f6d398be1c
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-07-08 14:39:23 +0300

    task 1.1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,31 @@
+'use strict';
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  'use strict';
+
+  arr = [];
+
+  let d;
+
+  d = b ** 2 - 4 * a * c;
+
+  if (d === 0) {
+    let d1 = - b / (2 * a);
+    arr.push(d1);
+
+  } else if (d > 0) {
+    let d2 = (- b + Math.sqrt(d)) / (2 * a);
+    let d3 = (- b - Math.sqrt(d)) / (2 * a);
+
+    arr.push(d2, d3);
+
+  } else {
+    arr = [];
+    
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
