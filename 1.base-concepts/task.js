66acd5d03de40eaf00355f461ca717959eeab132
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-07-10 01:58:50 +0300

    edit code style

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,7 +2,6 @@
 
 function solveEquation(a, b, c) {
   let arr;
-  'use strict';
 
   arr = [];
 
@@ -21,7 +20,7 @@ function solveEquation(a, b, c) {
     arr.push(+d2.toFixed(2), +d3.toFixed(2));
 
   } else {
-    arr = [];
+    arr;
 
   }
 
@@ -30,36 +29,33 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage( percent, contribution, amount, date ) {
   let totalAmount;
-
-  'use strict';
-
   let s;
   let p;
 
   if ( isNaN(amount) || amount == 0 || amount == '' || amount < 0 ) {
     return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`)
-  } else { s = amount - contribution }; 
+    } else { 
+      s = amount - contribution 
+  }; 
 
   if ( isNaN(contribution) || contribution < 0 ) {
     return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`)
   };
 
-  let current = new Date();
+  let currentDate = new Date();
 
   let n = ( date.getFullYear() * 12 + date.getMonth() ) -
-   ( current.getFullYear() * 12 + current.getMonth() ); 
+   ( currentDate.getFullYear() * 12 + currentDate.getMonth() ); 
 
   if ( isNaN(percent) || percent == '' || percent < 0 ) {
     return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`)
-  } else { p =( percent / 100 ) / 12 };
+    } else { 
+     p =( percent / 100 ) / 12 
+  };
 
-  let payment = s * ( p + ( p / ((( 1 + p ) ** n ) - 1 )));
+  let monthlyPayment = s * ( p + ( p / ((( 1 + p ) ** n ) - 1 )));
 
-  totalAmount = payment * n;  
+  totalAmount = monthlyPayment * n;  
 
   return +totalAmount.toFixed(2);
 }
-
-const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
-
-console.log(nextYearDate)
