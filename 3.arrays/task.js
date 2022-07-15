5f1abdcedd80e2b5416146b4db30c4e5decd5d57
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-07-15 17:25:34 +0300

    "task 3.1, 3.2 completed"

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
-  return result; // boolean
+  return (result =
+    arr1.length === arr2.length && arr1
+    .every((n, i) => n === arr2[i]));
+  // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
-  return resultArr; // array
+  return (resultArr = arr
+    .filter((element) => element > 0)
+    .filter((element) => element % 3 === 0)
+    .map((i) => i * 10));
+  //array
 }
