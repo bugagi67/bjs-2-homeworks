686a8b6217746ed94f80e19dd6365d47224e6c97
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-07-18 00:32:28 +0300

    task 4 completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = mark;
+  } else {
+    this.marks.push(...mark);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((acc, item) => acc + item, 0);
+  return sum / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+};
