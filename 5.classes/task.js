36d3bc179c7896c3ad15f4ff964bc7c2c12d2e53
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-07-22 14:15:01 +0300

    Task 5.1

---
+++ 5.classes/task.js
@@ -0,0 +1,63 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.type = null;
+    this.state = 100;
+  }
+
+  fix() {
+    let sum = this.state * 1.5;
+    this.state = sum;
+  }
+
+  set state(value) {
+    if (value < 0) {
+      return (this._state = 0);
+    } else if (value > 100) {
+      return (this._state = 100);
+    }
+    return (this._state = value);
+  }
+
+  get state() {
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
+}
