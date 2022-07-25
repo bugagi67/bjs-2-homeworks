14be585b166d7ccd47751c650f8ed67dbb453fd9
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-07-25 22:48:17 +0300

    Task 5.3

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -91,3 +91,77 @@ class Library {
     }
   }
 }
+
+class Student {
+  constructor(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.subject = [];
+  }
+
+  addSubject(subject) {
+    this.subject.push(subject);
+  }
+
+  addMark(mark, subjectName) {
+    let index = this.subject.findIndex(
+      (element) => element.name === subjectName
+    );
+    if (index === -1) {
+      return null;
+    } else if (mark > 0 && mark <= 5) {
+      this.subject[index].marks.push(mark);
+    } else {
+      console.log(`Ошибка, оценка должна быть числом от 1 до 5`);
+    }
+  }
+
+  getAverageBySubject(subjectName) {
+    let sum;
+    let index = this.subject.findIndex(
+      (element) => element.name === subjectName
+    );
+    if (index === -1) {
+      return console.log(`Несуществующий предмет`);
+    } else {
+      sum = this.subject[index].marks.reduce((acc, item) => acc + item, 0);
+    }
+    let average = sum / this.subject[index].marks.length;
+
+    return console.log(`Средний балл по предмету ${subjectName} ${average}`);
+  }
+
+  getAverage() {
+    let arrMarks = [];
+    let result = [];
+    for (let i = 0; i < this.subject.length; i++) {
+      for (let j = 0; j < this.subject[i].marks.length; j++) {
+        arrMarks.push(this.subject[i].marks[j]);
+      }
+    }
+    result = arrMarks.reduce((acc, item) => acc + item, 0);
+    return result / arrMarks.length;
+  }
+}
+
+class Algebra {
+  constructor(name) {
+    this.name = name;
+    this.marks = [];
+  }
+}
+
+class Geometry {
+  constructor(name) {
+    this.name = name;
+    this.marks = [];
+  }
+}
+
+class Physics {
+  constructor(name) {
+    this.name = name;
+    this.marks = [];
+  }
+}
