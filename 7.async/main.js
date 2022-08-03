2690d018603429e133341668ebdaa9f08835c12d
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-08-04 00:54:16 +0300

    task 7

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,47 @@
-// тут вы можете вызывать функции из task.js
+function testCase() {
+  let phoneAlarm = new AlarmClock();
+
+  setInterval(
+    () =>
+    phoneAlarm.addClock(
+      phoneAlarm.getCurrentFormattedTime(),
+      () => console.log("Пора вставать!"),
+      1
+    ), 2000
+  );
+
+  setTimeout(
+    () =>
+      phoneAlarm.addClock(
+        phoneAlarm.getCurrentFormattedTime(),
+        console.log("Давай, вставай уже!"),
+        2
+      )
+  );
+  setTimeout(
+    () =>
+      phoneAlarm.addClock(phoneAlarm.getCurrentFormattedTime(), () =>
+        console.log("Иди умываться!")
+      )
+  );
+  setTimeout(
+    () =>
+      phoneAlarm.addClock(
+        phoneAlarm.getCurrentFormattedTime(),
+        () => {
+          console.log("Вставай, а то проспишь!");
+          phoneAlarm.clearAlarms();
+          phoneAlarm.printAlarms();
+        },
+        3
+      )
+  );
+  phoneAlarm.addClock(
+    phoneAlarm.getCurrentFormattedTime(),
+    () => console.log("Вставай, а то проспишь!"),
+    1
+  );
+  phoneAlarm.printAlarms();
+  phoneAlarm.start();
+  phoneAlarm.stop();
+}
---
+++ 7.async/task.js
@@ -0,0 +1,69 @@
+"use strict";
+
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.timerId = null;
+  }
+
+  addClock(time, callback, id) {
+    if (id === undefined) {
+      throw new Error("ID не задан!");
+    } else if (this.alarmCollection.find((item) => item["id"] === id)) {
+      console.error("Будильник с данным ID уже существует!");
+      return;
+    } else {
+      const alarm = {
+        time,
+        callback,
+        id,
+      };
+      this.alarmCollection.push(alarm);
+    }
+  }
+
+  removeClock(id) {
+    let now = this.alarmCollection.filter((element) => element["id"] !== id);
+    this.alarmCollection = now;
+  }
+
+  getCurrentFormattedTime() {
+    let time = new Date().toLocaleTimeString().slice(0, -3);
+    return time;
+  }
+
+  start() {
+    function checkClock(clock) {
+      let now = new Date().toLocaleTimeString().slice(0, -3);
+      if (now === clock.time) {
+        clock[callback]();
+      }
+    }
+    if (this.timerId === null) {
+      let timerId = setInterval(() => {
+        this.alarmCollection.forEach((element) => checkClock(element));
+      }, 5000);
+      this.timerId = timerId;
+    }
+  }
+
+  stop() {
+    if (this.timerId !== undefined) {
+      clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
+
+  printAlarms() {
+    this.alarmCollection.forEach((element) =>
+      console.log(
+        "Будильник №" + element["id"] + " заведён на " + element["time"]
+      )
+    );
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection.splice(0, this.alarmCollection.length);
+  }
+}
